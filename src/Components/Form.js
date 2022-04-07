2100462e1e0aacceb6ba9d0a023a34adf7d283d6
ukasz1 <lukaszmitkowski53@gmail.com>
2022-04-07 16:53:22 +0200

    task and date inputs added

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,14 +1,51 @@
+import { days, months, years } from '../utils'
+
 const Form = () => {
   return <section className="form-container">
     <h4>Aby wprowadzić nowy event uzupełnij opis, określ deadline i naciśnij przycisk Dodaj.</h4>
     <form classname="event-form">
-      <label htmlFor="description">Opis </label>
-      <input
-        type="text"
-        name="description"
-        id="description"
-        className="form-description"
-      />
+      <div className="aspect">
+        <label htmlFor="description">Opis </label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          className="form-description"
+        />
+      </div>
+      <div className="aspect">
+        <label htmlFor="task">Rodzaj zadania </label>
+        <select
+          name="task"
+          id="task"
+          className="form-task"
+        >
+          <option value="work">Praca</option>
+          <option value="study">Nauka</option>
+          <option value="personal">Osobiste</option>
+          <option value="other">Inne</option>
+        </select>
+      </div>
+
+      <div className="aspect">
+        <label htmlFor="date">Data </label>
+        <select id="day" name="day">
+          {days.map((day, index) => {
+            return <option value={index}>{day}</option>
+          })}
+        </select>
+        <select id="month" name="month">
+          {months.map((month, index) => {
+            return <option value={index}>{month}</option>
+          })}
+        </select>
+        <select id="year" name="year">
+          {years.map((year, index) => {
+            return <option value={index}>{year}</option>
+          })}
+        </select>
+      </div>
+
     </form>
   </section>
 }
---
+++ src/utils.js
@@ -0,0 +1,23 @@
+const days = ['Dzień', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+const months = [
+  'Miesiąc',
+  'styczeń',
+  'luty',
+  'marzec',
+  'kwiecień',
+  'maj',
+  'czerwiec',
+  'lipiec',
+  'sierpień',
+  'wrzesień',
+  'październik',
+  'listopad',
+  'grudzień'
+]
+const years = ['Rok', 2022, 2023, 2024, 2025]
+
+export {
+  days,
+  months,
+  years
+}

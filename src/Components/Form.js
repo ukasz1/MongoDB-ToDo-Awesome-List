a28c61e0babaca29dacdbc5cda449e2315bebd4d
ukasz1 <lukaszmitkowski53@gmail.com>
2022-04-28 17:09:31 +0200

    graphical layout upgrade

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -82,109 +82,113 @@ const Form = () => {
       }
     }
   }
-  return <section className="form-container">
-    <h5>Aby wprowadzić nowy event uzupełnij opis, określ deadline i naciśnij przycisk Dodaj.</h5>
-    <form className="event-form" onSubmit={validateAndSubmitForm}>
-      <div className="aspect">
-        <label htmlFor="description">Opis </label>
-        <input
-          type="text"
-          name="description"
-          id="description"
-          className="form-description"
-          value={newEvent.description}
-          onChange={fillInForm}
-          onBlur={descriptionCheck}
-          required
-        />
-        {descriptionError ? <span className='err-msg'>Opis nie może być pusty!</span> : null}
-      </div>
-      <div className="aspect">
-        <label htmlFor="task">Rodzaj zadania </label>
-        <select
-          name="task"
-          id="task"
-          className="form-task"
-          value={newEvent.task}
-          onChange={fillInForm}
-        >
-          <option value="work">Praca</option>
-          <option value="study">Nauka</option>
-          <option value="personal">Osobiste</option>
-          <option value="other">Inne</option>
-        </select>
-      </div>
-      <div className="aspect">
-        <label htmlFor="date">Data </label>
-        <select
-          id="day"
-          name="day"
-          className="form-date"
-          value={newEvent.day}
-          onChange={fillInForm}
-        >
-          {days.map((day, index) => {
-            return <option key={index} value={index}>{day}</option>
-          })}
-        </select>
-        <select
-          id="month"
-          name="month"
-          className="form-date"
-          value={newEvent.month}
-          onChange={fillInForm}
-        >
-          {months.map((month, index) => {
-            return <option key={index} value={index}>{month}</option>
-          })}
-        </select>
-        <select
-          id="year"
-          name="year"
-          className="form-date"
-          value={newEvent.year}
-          onChange={fillInForm}
-        >
-          {years.map((year, index) => {
-            return <option key={index} value={index + 2021}>{year}</option>
-          })}
-        </select>
-        {dateError ? <span className='err-msg'>Niepoprawna data!</span> : null}
-      </div>
+  return <>
 
-      <div className='aspect'>
-        <label htmlFor="time">Godzina </label>
-        <input
-          type="text"
-          name="hour"
-          maxLength="2"
-          placeholder="hh"
-          pattern="[0-9]{1,}"
-          className='form-time'
-          value={newEvent.hour}
-          onChange={fillInForm}
-          onBlur={hourCheck}
-          required
-        />
-        <input
-          type="text"
-          name="minute"
-          maxLength="2"
-          placeholder="mm"
-          pattern="[0-9]{1,}"
-          className='form-time'
-          value={newEvent.minute}
-          onChange={fillInForm}
-          onBlur={minuteCheck}
-          required
-        />
-        {(hourError || minuteError) ? <span className='err-msg'>Niepoprawny czas!</span> : null}
-      </div>
-      <div className='form-submit-div'>
-        <button type="submit" className='submit-button'>Dodaj</button>
-      </div>
+    <section className="form-container">
+      <h5>Aby wprowadzić nowy event uzupełnij opis, określ deadline i naciśnij przycisk Dodaj.</h5>
+      <form className="event-form" onSubmit={validateAndSubmitForm}>
+        <div className="aspect">
+          <label htmlFor="description">Opis </label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            className="form-description"
+            value={newEvent.description}
+            onChange={fillInForm}
+            onBlur={descriptionCheck}
+            required
+          />
+          {descriptionError ? <span className='err-msg'>Opis nie może być pusty!</span> : null}
+        </div>
+        <div className="aspect">
+          <label htmlFor="task">Rodzaj zadania </label>
+          <select
+            name="task"
+            id="task"
+            className="form-task"
+            value={newEvent.task}
+            onChange={fillInForm}
+          >
+            <option value="work">Praca</option>
+            <option value="study">Nauka</option>
+            <option value="personal">Osobiste</option>
+            <option value="other">Inne</option>
+          </select>
+        </div>
+        <div className="aspect">
+          <label htmlFor="date">Data </label>
+          <select
+            id="day"
+            name="day"
+            className="form-date"
+            value={newEvent.day}
+            onChange={fillInForm}
+          >
+            {days.map((day, index) => {
+              return <option key={index} value={index}>{day}</option>
+            })}
+          </select>
+          <select
+            id="month"
+            name="month"
+            className="form-date"
+            value={newEvent.month}
+            onChange={fillInForm}
+          >
+            {months.map((month, index) => {
+              return <option key={index} value={index}>{month}</option>
+            })}
+          </select>
+          <select
+            id="year"
+            name="year"
+            className="form-date"
+            value={newEvent.year}
+            onChange={fillInForm}
+          >
+            {years.map((year, index) => {
+              return <option key={index} value={index + 2021}>{year}</option>
+            })}
+          </select>
+          {dateError ? <span className='err-msg'>Niepoprawna data!</span> : null}
+        </div>
 
-    </form>
-  </section >
+        <div className='aspect'>
+          <label htmlFor="time">Godzina </label>
+          <input
+            type="text"
+            name="hour"
+            maxLength="2"
+            placeholder="hh"
+            pattern="[0-9]{1,}"
+            className='form-time'
+            value={newEvent.hour}
+            onChange={fillInForm}
+            onBlur={hourCheck}
+            required
+          />
+          <input
+            type="text"
+            name="minute"
+            maxLength="2"
+            placeholder="mm"
+            pattern="[0-9]{1,}"
+            className='form-time'
+            value={newEvent.minute}
+            onChange={fillInForm}
+            onBlur={minuteCheck}
+            required
+          />
+          {(hourError || minuteError) ? <span className='err-msg'>Niepoprawny czas!</span> : null}
+        </div>
+        <div className='form-submit-div'>
+          <button type="submit" className='submit-button'>Dodaj</button>
+        </div>
+
+      </form>
+    </section >
+    <div className='empty-line'></div>
+  </>
 }
 export default Form;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 const Header = () => {
   return <div className="header-container">
     <h1>ToDo Awesome</h1>
+    <div className="underline"></div>
 
   </div>
 }
